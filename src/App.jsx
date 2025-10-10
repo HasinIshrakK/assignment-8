6c690132ea15e7bd951a5eed89694f7c2e077a3a
HasinIshrakK <hasinishrakk@gmail.com>
2025-10-10 17:40:04 +0600

    Loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router'
+import { Outlet, useNavigation } from 'react-router'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Footer from './Components/Footer/Footer'
@@ -10,11 +10,18 @@ export const ThemeContext = createContext([])
 function App() {
 
   const [theme, setTheme] = useState(true);
+  let navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
 
   return (
     <>
       <ThemeContext value={[theme, setTheme]}>
         <Navbar />
+        {
+          isLoading && <div className={`${theme === true && 'bg-[#F1F5E8]'} p-6 sm:p-20 flex justify-center`}>
+            <span className="loading loading-spinner text-info"></span>
+          </div>
+        }
         <Outlet></Outlet>
         <Footer></Footer>
       </ThemeContext>
